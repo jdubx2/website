efe0cd521230d4eb0cbd7c6fa95a01d55c7b3e3d
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-01-04 17:08:01 -0800

    Bootstrap initial commit

---
+++ bootstrap/vendor/bootstrap/js/newjs.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 20) {
+      $(".navbar").addClass("compressed");
+    } else {
+      $(".navbar").removeClass("compressed");
+    }
+  });
+});
