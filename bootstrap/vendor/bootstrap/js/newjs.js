9a36baf5b108ac81c4a7eb29114711a7cbb915bb
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-01-05 23:05:57 -0800

    navbar customization

--- bootstrap/vendor/bootstrap/js/newjs.js
+++ bootstrap/vendor/bootstrap/js/newjs.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     if ($(window).scrollTop() >= 20) {
       $(".navbar").addClass("compressed");
+      $(".navlogo").width(54).height(54);
+      $(".navbar-brand").addClass("compressed");
     } else {
       $(".navbar").removeClass("compressed");
+      $(".navlogo").width(65).height(65);
+      $(".navbar-brand").removeClass("compressed");
     }
   });
 });
