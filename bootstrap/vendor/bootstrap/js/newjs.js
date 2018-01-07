7de4954c6e3b8101e2598d3dc4f708a218f38c29
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-01-06 16:41:54 -0800

    Update background

--- bootstrap/vendor/bootstrap/js/newjs.js
+++ bootstrap/vendor/bootstrap/js/newjs.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     if ($(window).scrollTop() >= 20) {
       $(".navbar").addClass("compressed");
-      $(".navlogo").width(54).height(54);
+      $(".navlogo").width(42).height(42);
       $(".navbar-brand").addClass("compressed");
     } else {
       $(".navbar").removeClass("compressed");
-      $(".navlogo").width(65).height(65);
+      $(".navlogo").width(55).height(55);
       $(".navbar-brand").removeClass("compressed");
     }
   });
