ed7771dbf9248837c0a5f65f8541eabb1f5b16a3
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-01-28 19:40:35 -0800

    add facet plot

--- la_food/la_food.R
+++ la_food/la_food.R
@@ -14,25 +14,26 @@ densitys <- yelp %>%
          !(Parent2 == 'Markets' & Parent == 'Healthy')) %>% 
   ggplot(aes(x = weightedScore, fill = Parent2))+
   geom_density(alpha = .7) +
-  geom_vline(xintercept = .4, color = 'gray80', linetype = 'dashed', size = .5) +
+  geom_vline(xintercept = .4, color = 'gray80', linetype = 'dashed', size = .2) +
   facet_wrap(~Parent, ncol = 5) +
   theme_hc(bgcolor = "darkunica") +
   scale_fill_brewer(type = 'qual', palette = 'Set1') +
   theme(axis.text = element_text(color='gray80'),
         axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
-        text = element_text(family ='Calibri',size = 9),
+        text = element_text(family ='Calibri Light',size = 9),
         panel.background = element_rect(fill = '#211e1e', color = '#211e1e'),
         plot.background = element_rect(fill = '#211e1e', color = '#211e1e'),
         legend.background = element_rect(fill = '#211e1e', color = '#211e1e'),
         strip.background = element_rect(fill = '#211e1e', color = '#211e1e'),
-        strip.text = element_text(color = 'gray80', size = 10, family = 'Calibri Light'),
-        plot.margin = margin(0, .5, 0, .1, "cm")) +
+        strip.text = element_text(color = 'gray80', size = 9, family = 'Calibri Light'),
+        plot.margin = margin(.3, .5, 0, .1, "cm")) +
   guides(fill = guide_legend(nrow = 1)) +
   labs(x = 'Weighted Score',y = '', fill = '')
 
-ggsave(file="density.svg", plot=densitys, width=7.5, height=5)
+ggsave(file="density.svg", plot=densitys, width=8, height=4.5)
 
 
 library(geojsonio)
