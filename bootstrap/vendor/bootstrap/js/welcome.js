dd0b7ed9065aa40be7d32db85c1b75d560a0b60e
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-02-04 21:38:45 -0800

    revise banner

--- bootstrap/vendor/bootstrap/js/welcome.js
+++ bootstrap/vendor/bootstrap/js/welcome.js
@@ -8,8 +8,8 @@ d3.select('#svg8')
 
 let repeat = () => {
   var path = d3.selectAll('.svg_letter')
-  .attr('fill', 'rgba(185,185,185,0)')
-  // .attr('stroke', 'rgba(185,185,185,0)');
+  .attr('fill', 'rgba(16,16,16,0)')
+  // .attr('stroke', 'rgba(16,16,16,0)');
   .attr('stroke', '#2C7873');
 
   var totalLength = path.node().getTotalLength();
@@ -18,19 +18,36 @@ let repeat = () => {
     .attr("stroke-dashoffset", totalLength)
     .transition()
       .attr('stroke', '#2C7873')
-      .duration(5000)
+      .duration(1500)
       .ease(d3.easeLinear)
       .attr("stroke-dashoffset", 0)
     .transition()
-      .duration(2000)
-      .attr('fill', 'rgba(185,185,185,1)')
-      // .style('stroke-width', '.45px')
+      .duration(1000)
+      .attr('fill', 'rgba(16,16,16,1)')
       .ease(d3.easeLinear)
       .transition()
-      .delay(2000)
+      .delay(7000)
         .duration(2000)
-        .attr('fill', 'rgba(185,185,185,0)')
-        .attr('stroke', 'rgba(185,185,185,0)')
+        .attr('fill', 'rgba(16,16,16,0)')
+        .attr('stroke', 'rgba(16,16,16,0)')
+        .on("end", repeat);
+
+    var line = d3.selectAll('.underline')
+    var lineLength = line.node().getTotalLength();
+
+    line.attr("stroke-dasharray", lineLength + " " + lineLength)
+      .attr("stroke-dashoffset", lineLength)
+      .attr('stroke', '#2C7873')
+      .attr('fill', 'rgba(16,16,16,1)')
+      .transition()
+        .delay(1500)
+        .duration(1000)
+        .attr("stroke-dashoffset", 0)
+      .transition()
+        .delay(7000)
+        .duration(2000)
+        .attr('fill', 'rgba(16,16,16,0)')
+        .attr('stroke', 'rgba(16,16,16,0)')
         .on("end", repeat);
   }
 $(document).ready(repeat());
