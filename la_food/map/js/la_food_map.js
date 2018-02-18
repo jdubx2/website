eec4ad5d06b15a13839384baf2f996fd8e5f53a4
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-02-17 23:53:14 -0800

    add graphics to la_food

--- la_food/map/js/la_food_map.js
+++ la_food/map/js/la_food_map.js
@@ -11,18 +11,18 @@ var geojsonMarkerOptions = {
 var markerColor = d3.scaleSequential(d3.interpolatePiYG)
   .domain([.1,.6]);
 
-var map = new L.Map("map").setView([34.0522,-118.2437], 10);
+var map = new L.Map("map").setView([34.0022,-118.2437], 11);
 
 map.addLayer(new L.TileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"));
 
-d3.json("data/geo.json", function(error, collection) {
+d3.json("geo.json", function(error, collection) {
   if (error) throw error;
 
 L.geoJSON(collection, {
 
     pointToLayer: function (feature, latlng) {
         var fill_color = markerColor(feature.properties.weightedScore);
-        return L.circleMarker(latlng, {radius: 3.5, fillColor: fill_color, fillOpacity: 0.65,
+        return L.circleMarker(latlng, {radius: 4, fillColor: fill_color, fillOpacity: 0.65,
                                        color: "#000", weight : .2});
       },
       onEachFeature: function (feature, layer) {
